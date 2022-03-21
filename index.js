02745f3a66ea7e940cfe0a5baf1569e07125aea1
Justice39 <78258484+Justice39@users.noreply.github.com>
2022-03-21 17:17:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,97 +1,41 @@
 const Eris = require("eris");
 const axios = require("axios")
-const config = require("./config.json")
-const {token, vote_channel} = config
-const client = new Eris(token);
-
-console.log(token, vote_channel)
-client.on("messageCreate", async message => {
-    if(message.author.id === "504672489016918016") {
-        const text = `<@${client.user.id}>, formata uygun şekilde resimde verilen kodun bulunduğu butona basarak bu sunucuya oy verebilirsiniz. İşlem **60 saniye** sonra otomatik olarak iptal edilecek.`;
-        if(!message.content.includes(text)) return;
-        const components_data = message.components[0];
-        if (!components_data) return
-        //console.log(message.components[0]);
-        checkRealCode(components_data, message);
-
-    }
+const {token, channel_id} = require("./config.json")
+const client = new Eris(token, {
+    restMode: true
 });
 
-function checkRealCode(data, message) {
-    if(data.components == undefined || typeof data.components !== "object") return null
-    let real_button = null
+async function start(guild_id) {
+    const payload_json = {
+        "type": 2,
+        "application_id": "504672489016918016",
+        "guild_id": guild_id,
+        "channel_id": channel_id,
+        "session_id": "Justice39",
+        "data": {"version":"954369276008095777","id":"954369276008095774","name":"oy","type":1,"options":[],"application_command":{"application_id":"504672489016918016","default_member_permissions":null,"default_permission":true,"description":"Bu komut sayesinde sunucuya oy vererek destekte bulunabilirsiniz!","dm_permission":null,"id":"954369276008095774","name":"oy","permissions":[],"type":1,"version":"954369276008095777"},"attachments":[]}
+    };
     
-    for (let index = 0; index < data.components.length; index++) {
-        const button = data.components[index];
-        if(button.custom_id.endsWith("true")) {
-            real_button = button
-            sendCaptchaCode(real_button.custom_id, message)
-        }
-
-    }
-    if(real_button == null) return null
-    return real_button
-}
-
-function sendCaptchaCode(button_id, message) {
-    console.log("Button ID:",button_id);
-
     axios({
         url: "https://discord.com/api/v9/interactions",
         method: "POST",
-        headers: {authorization: token},
-        data: {
-            "application_id": "504672489016918016",
-            "message_id": message.id,
-            "message_flags": 0,
-            "channel_id": message.channel.id,
-            "guild_id": message.guildID,
-            "data": {
-                "component_type": 2,
-                "custom_id": button_id
-            },
-            "session_id": "e8de4493d638cd3eb217378978aa9cf9",
-            "type": 3
-        }
-    }).catch(err => console.log(err))
-}
+        headers: { authorization: token },
+        data: payload_json
+    }).catch(err=> console.log(err));
 
-async function start() {
-    client.createMessage(vote_channel,'!oy');
-
-    setTimeout(start, 3600000*2);
+    setTimeout(start, 7240000);
 }
 
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)) };
 
 
 client.connect().then(()=> {
-    const loop = setInterval(() =>{
+    const loop = setInterval(async () =>{
         if(client.guilds.size > 0) {
-            clearInterval(loop)
-            console.log("Ready!")
-            start()
+            clearInterval(loop);
+            console.log("Ready!");
+            const rest_channel = await client.getRESTChannel(channel_id);
+            
+            start(rest_channel.guild.id);
         }
     })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
