b37f03033987196e912730ef2627d3155f415e31
Justice39 <78258484+Justice39@users.noreply.github.com>
2022-03-21 17:23:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const Eris = require("eris");
 const axios = require("axios")
 const {token, channel_id} = require("./config.json")
-const client = new Eris(token, {
-    restMode: true
-});
+const client = new Eris(token, { restMode: true });
+let guildID;
 
-async function start(guild_id) {
+async function start() {
     const payload_json = {
         "type": 2,
         "application_id": "504672489016918016",
-        "guild_id": guild_id,
+        "guild_id": guildID,
         "channel_id": channel_id,
         "session_id": "Justice39",
         "data": {"version":"954369276008095777","id":"954369276008095774","name":"oy","type":1,"options":[],"application_command":{"application_id":"504672489016918016","default_member_permissions":null,"default_permission":true,"description":"Bu komut sayesinde sunucuya oy vererek destekte bulunabilirsiniz!","dm_permission":null,"id":"954369276008095774","name":"oy","permissions":[],"type":1,"version":"954369276008095777"},"attachments":[]}
@@ -34,8 +33,8 @@ client.connect().then(()=> {
             clearInterval(loop);
             console.log("Ready!");
             const rest_channel = await client.getRESTChannel(channel_id);
-            
-            start(rest_channel.guild.id);
+            guildID = rest_channel.guild.id 
+            start();
         }
     })
 })
